ea80d0ca9bce13f11e5f912d0f2220c728a3f0e4
seungju-mmc <ksj970213@hanyang.ac.kr>
2020-09-09 10:15:41 +0900

    Init

--- env_test.py
+++
@@ -1,6 +0,0 @@
-import gym
-
-
-env = gym.make('Hopper-v2')
-
-print(env.action_space,env.observation_space)
--- mujoco-py/mujoco_py/gl/glshim.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GLSHIM_H__
-#define __GLSHIM_H__
-
-int initOpenGL(int device_id);
-void closeOpenGL();
-int setOpenGLBufferSize(int width, int height);
-
-#endif
